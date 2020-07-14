27016bda495225af8fc31440f8acdbfc5fddc67d
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-07-14 21:00:56 +0300

    Change #97

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -1,6 +1,10 @@
-$(function() {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})
+// $(function() {
+//   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+//   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//     return new bootstrap.Tooltip(tooltipTriggerEl)
+//   })
+// })
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
