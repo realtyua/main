30d5b677ce843c93fbe228070b29a6f3b6544f16
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-07-14 23:39:41 +0300

    Change #102

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -1,18 +1,29 @@
-$(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-});
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
-function change_button(button_id, bool_val) {
-  var button_obj = document.getElementById(button_id);
-  button_obj.disabled = bool_val;
 
-  // Disable tooltip
-  if (bool_val) {
-    $('#' + button_id).tooltip('hide').tooltip('disable');
-  } else {
-    $('#' + button_id).tooltip('enable');
-  }
-}
+// $('a[data-toggle="tooltip"]').tooltip({
+//   animated: 'fade'
+// });
+
+
+// $(document).ready(function() {
+//   $('[data-toggle="tooltip"]').tooltip();
+// });
+//
+// function change_button(button_id, bool_val) {
+//   var button_obj = document.getElementById(button_id);
+//   button_obj.disabled = bool_val;
+//
+//   // Disable tooltip
+//   if (bool_val) {
+//     $('#' + button_id).tooltip('hide').tooltip('disable');
+//   } else {
+//     $('#' + button_id).tooltip('enable');
+//   }
+// }
 
 // $(function() {
 //   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
