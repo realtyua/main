b9ca716d338934461d162864d63e690d0cc016c5
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-07-16 17:12:56 +0300

    Change #140

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -3,3 +3,7 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+$('.nav-tabs>li>a.nav-link').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+})
