53e98f133d915f635b412b20629bae5b2afd9079
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-07-22 00:08:16 +0300

    Change #325

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -10,13 +10,16 @@ $('.nav-tabs>li>a.nav-link').on('click', function(){
 
 
 // Javascript to enable link to tab
-var hash = document.location.hash;
-var prefix = "tab_";
-if (hash) {
-    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href="#'+url.split('#')[1]+'"]').tab('show') ;
 }
 
-// Change hash for page-reload
-$('.nav-tabs a').on('shown', function (e) {
-    window.location.hash = e.target.hash.replace("#", "#" + prefix);
-});
+// With HTML5 history API, we can easily prevent scrolling!
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    if(history.pushState) {
+        history.pushState(null, null, e.target.hash);
+    } else {
+        window.location.hash = e.target.hash; //Polyfill for old browsers
+    }
+})
