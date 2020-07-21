b304d1012b88426e9f7af6c152a966adf830c8b7
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-07-21 23:46:14 +0300

    Change #322

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -7,3 +7,16 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 $('.nav-tabs>li>a.nav-link').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 })
+
+
+// // Javascript to enable link to tab
+// var hash = document.location.hash;
+// var prefix = "tab_";
+// if (hash) {
+//     $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+// }
+//
+// // Change hash for page-reload
+// $('.nav-tabs a').on('shown', function (e) {
+//     window.location.hash = e.target.hash.replace("#", "#" + prefix);
+// });
