ed183711441690c25308220641e8d7018a40fe8e
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-07-21 23:57:39 +0300

    Change #323

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -9,14 +9,14 @@ $('.nav-tabs>li>a.nav-link').on('click', function(){
 })
 
 
-// // Javascript to enable link to tab
-// var hash = document.location.hash;
-// var prefix = "tab_";
-// if (hash) {
-//     $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
-// }
-//
-// // Change hash for page-reload
-// $('.nav-tabs a').on('shown', function (e) {
-//     window.location.hash = e.target.hash.replace("#", "#" + prefix);
-// });
+// Javascript to enable link to tab
+var hash = document.location.hash;
+var prefix = "tab_";
+if (hash) {
+    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+}
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown', function (e) {
+    window.location.hash = e.target.hash.replace("#", "#" + prefix);
+});
