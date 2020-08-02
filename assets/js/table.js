3ba60517e9cbc6fe3ebcc318e2cab3764a24f6da
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-02 16:41:38 +0300

    Change #756

--- assets/js/table.js
+++ assets/js/table.js
@@ -54,6 +54,33 @@ function propertyDetailFormatter(value, row) {
     return html.join('')
 }
 
+function htmlDetailFormatter(value, row) {
+
+    // const images = Object.values(row.images || {})
+
+    let html = [
+        '<span class="row mx-0">',
+        '<span class="col-12 col-sm-6 col-md-4"><strong>Площа землі</strong>: ' + row.surface_land + ' м<sup>2</sup></span>',
+        '<span class="col-12 col-sm-6 col-md-4"><strong>Поверх</strong>: ' + row.floor + ' у ' + row.floors + ' поверховому будинку</span>',
+        '<span class="col-12 col-sm-6 col-md-4"><strong>Вартість</strong>: ' + row.price_sqmt + ' за 1 м<sup>2</sup></span>',
+        '<span class="col-12 col-sm-6 col-md-4"><strong>Стоя́нка</strong>: ' + row.parking + '</span>',
+        '<span class="col-12 col-sm-6 col-md-4"><strong>Доступна з</strong>: ' + row.date + '</span>',
+        '<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: <a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></span>',
+        '</span>',
+    ]
+
+
+    // if (images.length) {
+    //     html.push('<hr><span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">')
+    //     html.push(images.map(function (image) {
+    //         return '<span class="col px-1"><a href="/assets/images/' + row.phone + '/' + row.id + '/' + image.src + '" class="lightbox" title="Title Image Realestate" data-lightbox-width="1024" data-lightbox-height="768" data-lightbox-group="re-' + row.id + '4' + row.phone + '"><img src="/assets/images/' + row.phone + '/' + row.id + '/' + image.src + '" title="Title Image Realestate" alt="Alt Image Realestate" class="img-fluid img-thumbnail"></a></span>'
+    //     }).join(''))
+    //     html.push('</span>')
+    // }
+
+    return html.join('')
+}
+
 function priceSorter(a, b) {
   var aa = a.replace('$', '')
   var bb = b.replace('$', '')
