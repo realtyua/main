83ca401cdc7f5d4153f50519f323ff0676352637
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-05 21:40:17 +0300

    Change #755

--- assets/js/table.js
+++ assets/js/table.js
@@ -28,11 +28,8 @@ $(function() {
     $.each(row, function (key, value) {
       if (!key.startsWith('_') && key !== 'images' || key !== 'id' && value !== '') {
         if (value === 'Земля' || value === 'земля') {
-          if (key === 'price_sqmt') {
-            html.push('<span class="col-12 col-sm-6 col-md-4"><strong>Вартість за 1 м<sup>2</sup>:</strong> ' + row.surface_land + '</span>')
-          } else if (key === 'surface_land') {
-            html.push('<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: ' + row.seller + '</span>')
-          }
+          html.push('<span class="col-12 col-sm-6 col-md-4"><strong>Вартість за 1 м<sup>2</sup>:</strong> ' + row.surface_land + '</span>')
+          html.push('<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: ' + row.seller + '</span>')
         } else {
           if (key === 'floor') {
             html.push('<span class="col-12 col-sm-6 col-md-4"><strong>Поверх:</strong> ' + row.floor + ' у ' + row.floors + ' поверховому будинку</span>')
