2db45753422997a964a15134b80e41bfe1aa96ca
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-08 17:47:22 +0300

    Change #783

--- assets/js/table.js
+++ assets/js/table.js
@@ -33,7 +33,12 @@ $(function() {
     // var rowDate = row.date;
     // var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     // var valueDate = rowDate.toLocaleDateString("uk-UA", dateOptions);
-    // console.log(valueDate);
+
+    var rowDate = row.date;
+    function pad(n) {return n < 10 ? "0"+n : n;}
+    var valueDate = pad(rowDate.getDate())+"/"+pad(rowDate.getMonth()+1)+"/"+rowDate.getFullYear();
+    console.log(rowDat);
+    console.log(valueDate);
 
     // (if (row.price_sqmt.indexOf('$')) { (row.price_sqmt.replace('$','') * usd).toFixed(0) } else if (row.price_sqmt.indexOf('€')) { (row.price_sqmt.replace('€','') * eur).toFixed(0) } else { row.price_sqmt.toFixed(0) })
 
@@ -45,7 +50,7 @@ $(function() {
           html = [
             '<span class="row mx-0">',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>',
-            '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + '{{ site.data.lang-uk.re_uah }}</dd></dl></span>',
+            '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість землі за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Доступна з</dt><dd>' + row.date + '</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Продавець</dt><dd><a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></dd></dl></span>',
             '</span>',
@@ -55,7 +60,7 @@ $(function() {
             '<span class="row mx-0">',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Поверх</dt><dd>' + row.floor + ' у ' + row.floors + ' поверховому будинку</dd></dl></span>',
-            '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>',
+            '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Стоя́нка</dt><dd>' + row.parking + '</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Доступна з</dt><dd>' + row.date + '</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Продавець</dt><dd><a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></dd></dl></span>',
