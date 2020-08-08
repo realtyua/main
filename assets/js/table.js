634bc1030ecf414b4e863f357b02b8f8889f0cf7
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-08 13:12:36 +0300

    Change #766

--- assets/js/table.js
+++ assets/js/table.js
@@ -34,16 +34,25 @@ $(function() {
       if (!key.startsWith('_') && key !== 'images' || key !== 'id' && value !== '') {
 
         if (value.startsWith('Земля') || value.startsWith('земля')) {
-
           html = [
             '<span class="row mx-0">',
             '<span class="col-12 col-sm-6 col-md-4"><strong>Площа землі</strong>: ' + row.surface_land + ' м<sup>2</sup></span>',
-            '<span class="col-12 col-sm-6 col-md-4"><strong>Вартість за 1 м<sup>2</sup></strong>: ' + row.price_sqmt * usd + '</span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Вартість за 1 м<sup>2</sup></strong>: ' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + '</span>',
             '<span class="col-12 col-sm-6 col-md-4"><strong>Доступна з</strong>: ' + row.date + '</span>',
             '<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: <a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></span>',
             '</span>',
           ]
-
+        } else {
+          html = [
+            '<span class="row mx-0">',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Площа землі</strong>: ' + row.surface_land + ' м<sup>2</sup></span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Поверх</strong>: ' + row.floor + ' у ' + row.floors + ' поверховому будинку</span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Вартість за 1 м<sup>2</sup></strong>: ' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + '</span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Стоя́нка</strong>: ' + row.parking + '</span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Доступна з</strong>: ' + row.date + '</span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: <a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></span>',
+            '</span>',
+          ]
         }
 
       }
