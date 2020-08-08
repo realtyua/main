5f4248a35b354bd27be1c48c1986529b9a42635f
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-08 19:09:56 +0300

    Change #791

--- assets/js/table.js
+++ assets/js/table.js
@@ -38,20 +38,26 @@ $(function() {
 
         if (row.type.indexOf('Земля') !== -1 || row.type.indexOf('земля') !== -1) {
           html = [
-            //'<span class="row mx-0">',
+            '<span class="row mx-0">',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість землі за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Доступна з</dt><dd>' + row.date + '</dd></dl></span>',
             '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Продавець</dt><dd><a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></dd></dl></span>',
-            //'</span>',
+            '</span>',
           ]
         } else {
           if (row.surface_land !== '') {
-            html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>')
-          } else if (row.floor !== '') {
-            html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Поверх</dt><dd>' + row.floor + ' у ' + row.floors + ' поверховому будинку</dd></dl></span>')
+            html = [
+              '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>',
+            ]
+          } else if (row.floor !== '' || row.floors !== '') {
+            html = [
+              '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Поверх</dt><dd>' + row.floor + ' у ' + row.floors + ' поверховому будинку</dd></dl></span>',
+            ]
           } else if (row.price_sqmt !== '') {
-            html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+            html = [
+              '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>',
+            ]
           }
 
           // html = [
