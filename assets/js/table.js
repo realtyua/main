db765c89fcf0716400323884952d830d1fc01cca
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-08 19:16:30 +0300

    Change #792

--- assets/js/table.js
+++ assets/js/table.js
@@ -50,11 +50,13 @@ $(function() {
             html = [
               '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>',
             ]
-          } else if (row.floor !== '' || row.floors !== '') {
+          }
+          if (row.floor !== '' || row.floors !== '') {
             html = [
               '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Поверх</dt><dd>' + row.floor + ' у ' + row.floors + ' поверховому будинку</dd></dl></span>',
             ]
-          } else if (row.price_sqmt !== '') {
+          }
+          if (row.price_sqmt !== '') {
             html = [
               '<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>',
             ]
