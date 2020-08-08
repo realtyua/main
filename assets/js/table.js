5fb6f168cab65d9f600846c933161655afa6edf9
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-08 16:01:36 +0300

    Change #773

--- assets/js/table.js
+++ assets/js/table.js
@@ -31,7 +31,7 @@ $(function() {
     const images = Object.values(row.images || {});
 
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var date  = row.date;
+    var date  = row.date.toLocaleDateString("uk-UA", options);
 
     // (if (row.price_sqmt.indexOf('$')) { (row.price_sqmt.replace('$','') * usd).toFixed(0) } else if (row.price_sqmt.indexOf('€')) { (row.price_sqmt.replace('€','') * eur).toFixed(0) } else { row.price_sqmt.toFixed(0) })
 
@@ -43,7 +43,7 @@ $(function() {
             '<span class="row mx-0">',
             '<span class="col-12 col-sm-6 col-md-4"><strong>Площа землі</strong>: ' + row.surface_land + ' м<sup>2</sup></span>',
             '<span class="col-12 col-sm-6 col-md-4"><strong>Вартість за 1 м<sup>2</sup></strong>: ' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + '</span>',
-            '<span class="col-12 col-sm-6 col-md-4"><strong>Доступна з</strong>: ' + date.toLocaleDateString("uk-UA", options) + '</span>',
+            '<span class="col-12 col-sm-6 col-md-4"><strong>Доступна з</strong>: ' + date + '</span>',
             '<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: <a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></span>',
             '</span>',
           ]
