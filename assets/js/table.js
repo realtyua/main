88bd9cbfe00fb5f2e6e28f0696aafa9d8a625f2f
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-08 20:33:13 +0300

    Change #799

--- assets/js/table.js
+++ assets/js/table.js
@@ -47,14 +47,12 @@ $(function() {
           html = [
             '<span class="row mx-0">',
           ]
-
           if (row.surface_land !== '') {
             html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>')
           }
           if (row.floor !== '' && row.floors !== '') {
             html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Поверх</dt><dd>' + row.floor + ' у ' + row.floors + ' поверховому будинку</dd></dl></span>')
           }
-
           if (row.price_sqmt !== '' && row.price_sqmt.indexOf('$') !== -1) {
             html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
           } else if (row.price_sqmt !== '' && row.price_sqmt.indexOf('€') !== -1) {
@@ -62,7 +60,6 @@ $(function() {
           } else {
             html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + row.price_sqmt.toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
           }
-
           if (row.parking !== '') {
             html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Стоя́нка</dt><dd>' + row.parking + '</dd></dl></span>')
           }
@@ -72,7 +69,10 @@ $(function() {
           }
 
           if (row.phone !== '') {
-            html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Продавець</dt><dd><a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></dd></dl></span>')
+            var number = row.phone.replace(/[^\d]/g, "");
+            if (number.length != 12) { return 'Error number'; }
+            var phone = "+" + number.substr(0,2) + "&nbsp;" + number.substr(2,3) + "&nbsp;" + number.substr(5,3) + "&nbsp;" + number.substr(8,2) "&nbsp;" + number.substr(10,2);
+            html.push('<span class="col-12 col-sm-6 col-md-4"><dl><dt>Продавець</dt><dd><a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + phone + '</a></dd></dl></span>')
             html.push('</span>')
           }
 
@@ -152,33 +152,6 @@ function htmlPropertyFormatter(value, row) {
     return 'Продається <span class="text-lowercase"><strong>' + row.type + '</strong></span>, площею <strong>' + row.surface + '</strong> м<sup>2</sup>, кімнат <strong>' + row.rooms + '</strong>, знаходиться у <strong>' + row.location + '</strong> за адресою <strong>' + row.address + '</strong>.';
   }
 }
-
-// function propertyDetailFormatter(value, row) {
-//
-//     const images = Object.values(row.images || {})
-//
-//     let html = [
-//         '<span class="row mx-0">',
-//         '<span class="col-12 col-sm-6 col-md-4"><strong>Площа землі</strong>: ' + row.surface_land + ' м<sup>2</sup></span>',
-//         '<span class="col-12 col-sm-6 col-md-4"><strong>Поверх</strong>: ' + row.floor + ' у ' + row.floors + ' поверховому будинку</span>',
-//         '<span class="col-12 col-sm-6 col-md-4"><strong>Вартість</strong>: ' + row.price_sqmt + ' за 1 м<sup>2</sup></span>',
-//         '<span class="col-12 col-sm-6 col-md-4"><strong>Стоя́нка</strong>: ' + row.parking + '</span>',
-//         '<span class="col-12 col-sm-6 col-md-4"><strong>Доступна з</strong>: ' + row.date + '</span>',
-//         '<span class="col-12 col-sm-6 col-md-4"><strong>Продавець</strong>: <a href="tel:+' + row.phone + '" class="phone" title="' + row.seller + '">' + row.phone + '</a></span>',
-//         '</span>',
-//     ]
-//
-//
-//     if (images.length) {
-//         html.push('<hr><span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1 gallery re' + row.id + '">')
-//         html.push(images.map(function (image) {
-//             return '<span class="col px-1"><a href="/assets/images/' + row.phone + '/' + row.id + '/' + image.src + '" class="lightbox" title="Title Image Realestate" data-lightbox-width="1024" data-lightbox-height="768" data-lightbox-group="re-' + row.id + '4' + row.phone + '"><img src="/assets/images/' + row.phone + '/' + row.id + '/' + image.src + '" title="Title Image Realestate" alt="Alt Image Realestate" class="img-fluid img-thumbnail"></a></span>'
-//         }).join(''))
-//         html.push('</span>')
-//     }
-//
-//     return html.join('')
-// }
 
 function htmlDetailFormatter(value, row) {
 
