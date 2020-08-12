99bb7ec582a23e0a62a75aca3ebdf9d83997d12f
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-12 20:37:00 +0300

    Change #856

--- assets/js/table.js
+++ assets/js/table.js
@@ -2,18 +2,91 @@
 layout: null
 ---
 
+$(function () {
+
+    "use strict";
+
+    var expandedRow = null;
+
+    $('table').on('expand-row.bs.table', function (event, index) {
+        if (expandedRow !== index) {
+            $('table').bootstrapTable('collapseRow', expandedRow);
+        }
+        expandedRow = index;
+    });
+
+    $('table').on('click-row.bs.table', function (e, row, $element) {
+        $($element).siblings().removeClass('active');
+        $($element).addClass('active');
+    });
+
+});
+
+var month = ["{{ site.data.lang-uk.m_01 }}", "{{ site.data.lang-uk.m_02 }}", "{{ site.data.lang-uk.m_03 }}", "{{ site.data.lang-uk.m_04 }}", "{{ site.data.lang-uk.m_05 }}", "{{ site.data.lang-uk.m_06 }}", "{{ site.data.lang-uk.m_07 }}", "{{ site.data.lang-uk.m_08 }}", "{{ site.data.lang-uk.m_09 }}", "{{ site.data.lang-uk.m_10 }}", "{{ site.data.lang-uk.m_11 }}", "{{ site.data.lang-uk.m_12 }}"];
+var usd = {{ site.usd }};
+var eur = {{ site.eur }};
 var items = [];
 
-function jsDetailFormatter(index, row, $element) {
+function jsDetailFormatter(index, row, $detail) {
 
   "use strict";
 
   var html = [];
 
-  $.each(row, function (key, value) {
-      if (key !== 'images' && value !== '') {
-          html.push('<p><b>' + key + ':</b> ' + '<a class="value" href="#" data-type="text">' + value + '</a>' + '</p>')
+  $.each(row, function (index, key, value) {
+    if (key !== 'images' || key !== 'id' && value !== '') {
+      if (row.type.indexOf('Земля') !== -1 || row.type.indexOf('земля') !== -1) {
+        html = [
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">',
+          '<span class="col px-1"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>',
+        ]
+        if (row.price_sqmt !== '' && row.price_sqmt.indexOf('$') !== -1) {
+          html.push('<span class="col px-1"><dl><dt>Вартість землі за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+        } else if (row.price_sqmt !== '' && row.price_sqmt.indexOf('€') !== -1) {
+          html.push('<span class="col px-1"><dl><dt>Вартість землі за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('€','') * eur).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+        } else {
+          html.push('<span class="col px-1"><dl><dt>Вартість землі за 1 м<sup>2</sup></dt><dd>' + row.price_sqmt.toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+        }
+        if (row.date !== '') {
+          var d = new Date(row.date);
+          var n = d.getMonth();
+          html.push('<span class="col px-1"><dl><dt>Нерухомість доступна з</dt><dd>' + d.getDate() + '&nbsp;' + month[n] + '&nbsp;' + d.getFullYear() + '&nbsp;{{ site.data.lang-uk.roku }}</dd></dl></span>'),
+          html.push('<span class="col px-1"><dl><dt>Продавець</dt><dd>' + row.seller + '</dd></dl></span>'),
+          html.push('<span class="col px-1"><dl><dt>Контакти продавця</dt><dd><a href="tel:+' + row.phone + '">+' + row.phone.substr(0, 2) + '&nbsp;' + row.phone.substr(2, 3) + '&nbsp;' + row.phone.substr(5, 3) + '&nbsp;' + row.phone.substr(8, 2) + '&nbsp;' + row.phone.substr(10, 2) + '</a></dd></dl></span>'),
+          html.push('</span>')
+        }
+      } else {
+        html = [
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">',
+        ]
+        if (row.surface_land !== '') {
+          html.push('<span class="col px-1"><dl><dt>Площа землі</dt><dd>' + row.surface_land + ' м<sup>2</sup></dd></dl></span>')
+        }
+        if (row.floor !== '' && row.floors !== '') {
+          html.push('<span class="col px-1"><dl><dt>Поверх</dt><dd>' + row.floor + ' у ' + row.floors + ' поверховому будинку</dd></dl></span>')
+        }
+        if (row.price_sqmt !== '' && row.price_sqmt.indexOf('$') !== -1) {
+          html.push('<span class="col px-1"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('$','') * usd).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+        } else if (row.price_sqmt !== '' && row.price_sqmt.indexOf('€') !== -1) {
+          html.push('<span class="col px-1"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + (row.price_sqmt.replace('€','') * eur).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+        } else {
+          html.push('<span class="col px-1"><dl><dt>Вартість нерухомості за 1 м<sup>2</sup></dt><dd>' + row.price_sqmt.toFixed(0) + ' {{ site.data.lang-uk.re_uah }}</dd></dl></span>')
+        }
+        if (row.parking !== '') {
+          html.push('<span class="col px-1"><dl><dt>Стоя́нка</dt><dd>' + row.parking + '</dd></dl></span>')
+        }
+        if (row.date !== '') {
+          var d = new Date(row.date);
+          var n = d.getMonth();
+          html.push('<span class="col px-1"><dl><dt>Нерухомість доступна з</dt><dd>' + d.getDate() + '&nbsp;' + month[n] + '&nbsp;' + d.getFullYear() + '&nbsp;{{ site.data.lang-uk.roku }}</dd></dl></span>')
+        }
+        if (row.phone !== '') {
+          html.push('<span class="col px-1"><dl><dt>Продавець</dt><dd>' + row.seller + '</dd></dl></span>'),
+          html.push('<span class="col px-1"><dl><dt>Контакти продавця</dt><dd><a href="tel:+' + row.phone + '">+' + row.phone.substr(0, 2) + '&nbsp;' + row.phone.substr(2, 3) + '&nbsp;' + row.phone.substr(5, 3) + '&nbsp;' + row.phone.substr(8, 2) + '&nbsp;' + row.phone.substr(10, 2) + '</a></dd></dl></span>'),
+          html.push('</span>')
+        }
       }
+    }
   })
 
   const images = Object.values(row.images || {});
@@ -26,11 +99,11 @@ function jsDetailFormatter(index, row, $element) {
     html.push('</span>')
   }
 
-  $element.html(html.join(''))
+  $detail.html(html.join(''))
 
   items[index] = [];
 
-  $element.find('figure').each(function(){
+  $detail.find('figure').each(function(){
     var $link = $(this).find('a');
     items[index].push({
         src: $link.attr('href'),
@@ -56,7 +129,6 @@ function htmlDetailFormatter(value, row) {
   return html.join('')
 
 }
-
 
 $(document).on('click', '.lightbox', function(event){
   event.preventDefault();
