2442d85b249e711987786306f706727936a3ad1c
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-15 18:15:36 +0300

    Change #908

--- assets/js/table.js
+++ assets/js/table.js
@@ -3,23 +3,18 @@ layout: null
 ---
 
 $(function () {
-
-    "use strict";
-
-    var expandedRow = null;
-
-    $('table').on('expand-row.bs.table', function (event, index) {
-        if (expandedRow !== index) {
-            $('table').bootstrapTable('collapseRow', expandedRow);
-        }
-        expandedRow = index;
-    });
-
-    $('table').on('click-row.bs.table', function (e, row, $element) {
-        $($element).siblings().removeClass('active');
-        $($element).addClass('active');
-    });
-
+  "use strict";
+  var expandedRow = null;
+  $('table').on('expand-row.bs.table', function (event, index) {
+    if (expandedRow !== index) {
+      $('table').bootstrapTable('collapseRow', expandedRow);
+    }
+    expandedRow = index;
+  });
+  $('table').on('click-row.bs.table', function (e, row, $element) {
+    $($element).siblings().removeClass('active');
+    $($element).addClass('active');
+  });
 });
 
 var month = ["{{ site.data.lang-uk.m_01 }}","{{ site.data.lang-uk.m_02 }}","{{ site.data.lang-uk.m_03 }}","{{ site.data.lang-uk.m_04 }}","{{ site.data.lang-uk.m_05 }}","{{ site.data.lang-uk.m_06 }}","{{ site.data.lang-uk.m_07 }}","{{ site.data.lang-uk.m_08 }}","{{ site.data.lang-uk.m_09 }}","{{ site.data.lang-uk.m_10 }}","{{ site.data.lang-uk.m_11 }}","{{ site.data.lang-uk.m_12 }}"];
