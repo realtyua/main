41a490cd6b584fcd7b88eeff9bb5a931865c069e
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-15 19:56:56 +0300

    Change #916

--- assets/js/table.js
+++ assets/js/table.js
@@ -315,9 +315,19 @@ function priceFormatter(value) {
 
 function priceSorter(a, b) {
   "use strict";
-  var aa = a.replace('гривень', '');
-  var bb = b.replace('гривень', '');
-  return aa - bb
+  if (a.indexOf('$') !== -1 && b.indexOf('$') !== -1) {
+    var aa = a.replace('$', '');
+    var bb = b.replace('$', '');
+    return aa - bb
+  } else if (a.indexOf('€') !== -1 && b.indexOf('€') !== -1) {
+    var aa = a.replace('€', '');
+    var bb = b.replace('€', '');
+    return aa - bb
+  } else {
+    var aa = a;
+    var bb = b;
+    return aa - bb
+  }
 }
 
 // function priceSorter(a, b) {
