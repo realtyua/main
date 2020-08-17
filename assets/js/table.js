1f7bea3d1805a3da5312cbc999ede6a30841da8d
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-08-17 20:29:47 +0300

    Change #978

--- assets/js/table.js
+++ assets/js/table.js
@@ -342,14 +342,14 @@ function propertyFormatter(value, row) {
 function priceFormatter(value, row) {
   "use strict";
   if (value !== '' && value.indexOf('$') !== -1) {
-    return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('$','') * usd).toFixed(0) + '</span> {{ site.data.lang-uk.re_uah }}';
+    return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('$','') * usd).toFixed(0) + '&nbsp;{{ site.data.lang-uk.re_uah }}</span>';
   } else if (value !== '' && value.indexOf('€') !== -1) {
-    return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('€','') * eur).toFixed(0) + '</span> {{ site.data.lang-uk.re_uah }}';
+    return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('€','') * eur).toFixed(0) + '&nbsp;{{ site.data.lang-uk.re_uah }}</span>';
   } else if (value === '' && row.price_sqmt !== '') {
     if (row.price_sqmt !== '' && row.price_sqmt.indexOf('$') !== -1) {
-      return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('$','') * usd).toFixed(0) + '</span> {{ site.data.lang-uk.re_uah }}';
+      return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('$','') * usd).toFixed(0) + '&nbsp;{{ site.data.lang-uk.re_uah }}</span>';
     } else if (row.price_sqmt !== '' && row.price_sqmt.indexOf('€') !== -1) {
-      return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('€','') * eur).toFixed(0) + '</span> {{ site.data.lang-uk.re_uah }}';
+      return '<span data-toggle="tooltip" title="' + value + '">' + (value.replace('€','') * eur).toFixed(0) + '&nbsp;{{ site.data.lang-uk.re_uah }}</span>';
     } else if (row.price_sqmt !== '') {
       return (row.price_sqmt*1).toFixed(0) + ' {{ site.data.lang-uk.re_uah }}';
     }
