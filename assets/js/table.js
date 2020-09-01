64e9c7e4fffc4cd792fcc0c040411ad87a235a9f
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-09-01 11:37:35 +0300

    Change #1249

--- assets/js/table.js
+++ assets/js/table.js
@@ -2,6 +2,13 @@
 layout: null
 ---
 
+var $table = $('#table');
+
+$(function() {
+  var data = [{"id":"0","type":"Квартира","rent":"","rooms":"1","surface":"46","surface_land":"","price":"$15000","price_sqmt":"$450","floor":"3","floors":"10","parking":"","region":"","location":"м. Івано-Франківськ","address":"вул. Івасюка","object":"Бульвар Європейський","date":"2020-08-15","seller":"Будівельна компанія 'Вамбуд'","phone":"380504337722","description":"","images":""},{"id":"1","type":"Нежитлове приміщення","rent":"","rooms":"","surface":"300","surface_land":"25","price":"€120000","price_sqmt":"€400","floor":"1","floors":"8","parking":"Власна прибудинкова територія","region":"","location":"м. Івано-Франківськ","address":"вул. Івасюка","object":"Бульвар Європейський","date":"2020-08-15","seller":"Будівельна компанія 'Вамбуд'","phone":"380504337722","description":"","images":[{"src":"image1.jpg","alt":"image1 alt","title":"image1 title"},{"src":"image3.jpg","alt":"image3 alt","title":"image3 title"}]},{"id":"4","type":"Квартира","rent":"","rooms":"4","surface":"80","surface_land":"","price":"$100000","price_sqmt":"$650","floor":"2","floors":"2","parking":"Гараж","region":"","location":"м. Івано-Франківськ","address":"вул. Івасюка","object":"Бульвар Європейський","date":"2020-08-15","seller":"Будівельна компанія 'Вамбуд'","phone":"380504337722","description":"","images":""},{"id":"5","type":"Квартира","rent":"","rooms":"2","surface":"41","surface_land":"","price":"$20000","price_sqmt":"$650","floor":"2","floors":"8","parking":"Прибудинкова територія","region":"","location":"м. Івано-Франківськ","address":"вул. Івасюка","object":"Бульвар Європейський","date":"2020-08-15","seller":"Будівельна компанія 'Вамбуд'","phone":"380504337722","description":"","images":""},{"id":"6","type":"Квартира","rent":"","rooms":"2","surface":"65","surface_land":"","price":"$110000","price_sqmt":"","floor":"2","floors":"8","parking":"Прибудинкова територія","region":"","location":"м. Івано-Франківськ","address":"вул. Івасюка","object":"Бульвар Європейський","date":"2020-08-15","seller":"Будівельна компанія 'Вамбуд'","phone":"380504337722","description":"","images":""}];
+  $table.bootstrapTable({data: data})
+});
+
 $(function () {
   "use strict";
   var expandedRow = null;
@@ -53,7 +60,6 @@ $(function () {
     $($element).siblings().removeClass('active');
     $($element).addClass('active');
   });
-  //dummy ();
 });
 
 var month = ["{{ site.data.lang-uk.m_01 }}","{{ site.data.lang-uk.m_02 }}","{{ site.data.lang-uk.m_03 }}","{{ site.data.lang-uk.m_04 }}","{{ site.data.lang-uk.m_05 }}","{{ site.data.lang-uk.m_06 }}","{{ site.data.lang-uk.m_07 }}","{{ site.data.lang-uk.m_08 }}","{{ site.data.lang-uk.m_09 }}","{{ site.data.lang-uk.m_10 }}","{{ site.data.lang-uk.m_11 }}","{{ site.data.lang-uk.m_12 }}"];
@@ -415,8 +421,3 @@ function priceSorter(a, b) {
   var bb = b.replace(s, '');
   return aa - bb
 }
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});
