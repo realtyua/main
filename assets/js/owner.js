7452a70d537f52bd7f955c98ac4dc512498de6e1
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-09-20 18:03:32 +0300

    Change #1627

--- assets/js/owner.js
+++ assets/js/owner.js
@@ -22,21 +22,37 @@
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#navbarOwner',
+    target: '#mainNav',
     offset: 75
   });
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#navbarOwner").offset().top > 100) {
-      $("#navbarOwner").addClass("navbar-scrolled");
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
     } else {
-      $("#navbarOwner").removeClass("navbar-scrolled");
+      $("#mainNav").removeClass("navbar-scrolled");
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+
+  // Magnific popup calls
+  $('#portfolio').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
 
 })(jQuery); // End of use strict
