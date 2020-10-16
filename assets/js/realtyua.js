476198673d99ace7b13acc4eed657a2ecf9a3fb9
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-10-16 22:16:20 +0300

    Change #1853

--- assets/js/realtyua.js
+++ assets/js/realtyua.js
@@ -38,19 +38,23 @@ function generateRandomRe() {
 
                 if (counter == (number - 1)) {
                   if (data[i].type === 'Квартира') {
-                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю квартиру загальною площею ' + data[i].surface + ' м<sup>2</sup></p></div></div>');
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю квартиру загальною площею ' + data[i].surface + '&nbsp;м<sup>2</sup></p></div></div>');
                   } else if (data[i].type === 'Частина будинку') {
-                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю частину будинку загальною площею ' + data[i].surface + ' м<sup>2</sup></p></div></div>');
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю частину будинку загальною площею ' + data[i].surface + '&nbsp;м<sup>2</sup></p></div></div>');
+                  } else if (data[i].type === 'Земля') {
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю землю ' загальною площею ' + data[i].surface_land + '&nbsp;м<sup>2</sup></p></div></div>');
                   } else {
-                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю ' + data[i].type + ' загальною площею ' + data[i].surface + ' м<sup>2</sup></p></div></div>');
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю ' + data[i].type + ' загальною площею ' + data[i].surface + '&nbsp;м<sup>2</sup></p></div></div>');
                   }
                 } else {
                   if (data[i].type === 'Квартира') {
-                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю квартиру загальною площею ' + data[i].surface + ' м<sup>2</sup></p></div></div>');
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю квартиру загальною площею ' + data[i].surface + '&nbsp;м<sup>2</sup></p></div></div>');
                   } else if (data[i].type === 'Частина будинку') {
-                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю частину будинку загальною площею ' + data[i].surface + ' м<sup>2</sup></p></div></div>');
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю частину будинку загальною площею ' + data[i].surface + '&nbsp;м<sup>2</sup></p></div></div>');
+                  } else if (data[i].type === 'Земля') {
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю землю ' загальною площею ' + data[i].surface_land + '&nbsp;м<sup>2</sup></p></div></div>');
                   } else {
-                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю ' + data[i].type + ' загальною площею ' + data[i].surface + ' м<sup>2</sup></p></div></div>');
+                    div.append('<div class="card"><div class="card-body"><p class="card-text">Продаю ' + data[i].type + ' загальною площею ' + data[i].surface + '&nbsp;м<sup>2</sup></p></div></div>');
                   }
                 }
 
