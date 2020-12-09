9d412cea5c3a314f4d917164912a8d3836eab844
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-12-09 23:59:54 +0200

    Change #2359

--- _includes/js/realtyua.js
+++ _includes/js/realtyua.js
@@ -1,9 +1,10 @@
 
 $(document).ready(function(){
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
-console.log(tooltipTriggerList);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl) });
-console.log(tooltipList);
+$('[data-toggle="tooltip"]').tooltip(); 
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+// console.log(tooltipTriggerList);
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl) });
+// console.log(tooltipList);
 $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
