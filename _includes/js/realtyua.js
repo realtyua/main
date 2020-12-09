add7c00ac7ecd7ac2d3379096c84fa86534afa97
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-12-09 23:03:21 +0200

    Change #2357

--- _includes/js/realtyua.js
+++ _includes/js/realtyua.js
@@ -1,5 +1,7 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+console.log(tooltipTriggerList);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl) });
+console.log(tooltipList);
 $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
