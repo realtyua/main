23199aa0074c02a69c526f44577fabaf40d30010
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2020-12-13 17:41:05 +0200

    Change #2431

--- assets/js/table.js
+++ assets/js/table.js
@@ -76,7 +76,7 @@ function jsDetailFormatter(index, row, $detail) {
     if (key !== 'images' || key !== 'id' && value !== '') {
       if (row.type.indexOf('{{ site.data.lang-uk.re_land }}') !== -1 || row.type.indexOf('{{ site.data.lang-uk.re_land | downcase }}') !== -1) {
         html = [
-          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">',
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">',
           '<span class="col px-1"><dl><dt>{{ site.data.lang-uk.re_surface_land }}</dt><dd>' + row.surface_land + ' {{ site.data.lang-uk.re_m }}<sup>2</sup></dd></dl></span>',
         ]
         if (row.price_sqmt !== '' && row.price_sqmt.indexOf('$') !== -1) {
@@ -102,7 +102,7 @@ function jsDetailFormatter(index, row, $detail) {
         }
       } else if (row.rent && row.rent !== '' && row.rent === '1') {
         html = [
-          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">'
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">'
         ]
         if (row.floor !== '' && row.floors !== '' && row.type.indexOf('{{ site.data.lang-uk.re_house }}') === -1) {
           html.push('<span class="col px-1"><dl><dt>{{ site.data.lang-uk.re_floor }}</dt><dd>' + row.floor + ' {{ site.data.lang-uk.re_at }} ' + row.floors + ' {{ site.data.lang-uk.re_floors }}</dd></dl></span>')
@@ -136,7 +136,7 @@ function jsDetailFormatter(index, row, $detail) {
         }
       } else {
         html = [
-          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">',
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">',
         ]
         if (row.surface_land !== '') {
           html.push('<span class="col px-1"><dl><dt>{{ site.data.lang-uk.re_surface_land }}</dt><dd>' + row.surface_land + ' {{ site.data.lang-uk.re_m }}<sup>2</sup></dd></dl></span>')
@@ -180,7 +180,7 @@ function jsDetailFormatter(index, row, $detail) {
   const images = Object.values(row.images || {});
 
   if (images.length) {
-    html.push('<hr><span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">'),
+    html.push('<hr><span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">'),
     html.push(images.map(function (image) {
       return '<figure class="col px-1"><a href="/assets/images/' + row.phone + '/' + row.id + '/' + image.src + '" class="lightbox" title="Title Image Realestate" data-lightbox-width="1024" data-lightbox-height="768" data-lightbox-group="re-' + row.id + '4' + row.phone + '"><img src="/assets/images/' + row.phone + '/' + row.id + '/' + image.src + '" loading="lazy" title="Title Image Realestate" alt="Alt Image Realestate" class="img-fluid img-thumbnail" intrinsicsize="1024x768"></a></figure>'
     }).join('')),
@@ -217,7 +217,7 @@ function htmlDetailFormatter(index, row, $detail) {
     if (key !== 'images' || key !== 'id' && value !== '') {
       if (row.type.indexOf('{{ site.data.lang-uk.re_land }}') !== -1 || row.type.indexOf('{{ site.data.lang-uk.re_land | downcase }}') !== -1) {
         html = [
-          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">',
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">',
           '<span class="col px-1"><dl><dt>{{ site.data.lang-uk.re_surface_land }}</dt><dd>' + row.surface_land + ' {{ site.data.lang-uk.re_m }}<sup>2</sup></dd></dl></span>',
         ]
         if (row.price_sqmt !== '' && row.price_sqmt.indexOf('$') !== -1) {
@@ -243,7 +243,7 @@ function htmlDetailFormatter(index, row, $detail) {
         }
       } else if (row.rent && row.rent !== '' && row.rent === '1') {
         html = [
-          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">'
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">'
         ]
         if (row.floor !== '' && row.floors !== '' && row.type.indexOf('{{ site.data.lang-uk.re_house }}') === -1) {
           html.push('<span class="col px-1"><dl><dt>{{ site.data.lang-uk.re_floor }}</dt><dd>' + row.floor + ' {{ site.data.lang-uk.re_at }} ' + row.floors + ' {{ site.data.lang-uk.re_floors }}</dd></dl></span>')
@@ -277,7 +277,7 @@ function htmlDetailFormatter(index, row, $detail) {
         }
       } else {
         html = [
-          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">',
+          '<span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">',
         ]
         if (row.surface_land !== '') {
           html.push('<span class="col px-1"><dl><dt>{{ site.data.lang-uk.re_surface_land }}</dt><dd>' + row.surface_land + ' {{ site.data.lang-uk.re_m }}<sup>2</sup></dd></dl></span>')
@@ -319,7 +319,7 @@ function htmlDetailFormatter(index, row, $detail) {
   })
 
   if (images.length) {
-    html.push('<hr><span class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-n1">'),
+    html.push('<hr><span class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mx-n1">'),
     html.push(images.map(function (image) {
       return '<figure class="col px-1"><a href="' + image + '" class="lightbox" title="Title Image Realestate" data-lightbox-width="1024" data-lightbox-height="768" data-lightbox-group="re-' + row.id + '4' + row.phone + '"><img src="' + image + '" loading="lazy" title="Title Image Realestate" alt="Alt Image Realestate" class="img-fluid img-thumbnail" intrinsicsize="1024x768"></a></figure>'
     }).join('')),
