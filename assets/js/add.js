d7a6a676b991b0249242ce85a566c19d3130a0b5
Vasyl Mosiychuk <vasyl@typo3.net.ua>
2021-01-10 17:21:50 +0200

    Change #2726

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,19 +1,46 @@
-
 (function() {
-    "use strict";
-    window.addEventListener("load", function() {
-        var forms = document.getElementsByClassName("needs-validation");
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener("submit", function(event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add("was-validated");
-            }, false);
-        });
-    }, false);
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          form.classList.add('was-validated');
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          // still prevent the form from actually submitting, as we're not REALLY doing that - we're AJAXing
+          event.preventDefault();
+          event.stopPropagation();
+          // do your AJAX thing ...
+          // then clear the form
+          form.classList.remove('was-validated');
+          form.reset();
+        }
+      }, false);
+    });
+  }, false);
 })();
+
+
+
+// (function() {
+//     "use strict";
+//     window.addEventListener("load", function() {
+//         var forms = document.getElementsByClassName("needs-validation");
+//         var validation = Array.prototype.filter.call(forms, function(form) {
+//             form.addEventListener("submit", function(event) {
+//                 if (form.checkValidity() === false) {
+//                     event.preventDefault();
+//                     event.stopPropagation();
+//                 }
+//                 form.classList.add("was-validated");
+//             }, false);
+//         });
+//     }, false);
+// })();
 
 $(document).ready(function() {
   var form = document.getElementById('add-form');
