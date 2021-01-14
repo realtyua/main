c677d69373d958373815fdfdd9f40d66cbeecf04
typo3ua <vasyl@typo3.net.ua>
2021-01-14 17:33:53 +0200

    Change #2802

--- _includes/js/table.js
+++ _includes/js/table.js
@@ -314,9 +314,10 @@ function propertyFormatter(value, row) {
     if (row.type.indexOf('{{ site.data.lang-uk.re_land }}') !== -1 || row.type.indexOf('{{ site.data.lang-uk.re_land | downcase }}') !== -1) {
       html = ['{{ site.data.lang-uk.re_for_sale }} <strong class="text-lowercase">' + row.type + '</strong>, ']
       if (row.surface_land && row.surface_land !== '') { html.push('{{ site.data.lang-uk.re_surface }} <strong>' + row.surface_land + ' {{ site.data.lang-uk.re_m }}</strong><sup>2</sup>') }
-      if (row.location && row.location !== '') { html.push(', ' + row.location + '') }
-      if (row.address && row.address !== '') { html.push(', {{ site.data.lang-uk.re_location }} <strong>' + row.address + '</strong>') }
-      if (row.region && row.region !== '') { html.push(', ' + row.region + '.') }
+      if (row.location && row.location !== '') { html.push(', {{ site.data.lang-uk.re_location }} <strong>' + row.location + '</strong>') }
+      if (row.address && row.address !== '' && row.location !== '') { html.push(', {{ site.data.lang-uk.re_address }} <strong>' + row.address + '</strong>') }
+      if (row.address && row.address !== '' && row.location === '') { html.push(', {{ site.data.lang-uk.re_location }} {{ site.data.lang-uk.re_address }} <strong>' + row.address + '</strong>') }
+      if (row.region && row.region !== '' && row.location === '') { html.push(', ' + row.region + '.') }
     } else if (row.rent !== '' && row.rent === '1' && row.price !== '') {
       html = ['{{ site.data.lang-uk.re_for_rent }} <strong class="text-lowercase">' + row.type + '</strong>, ']
       if (row.surface && row.surface !== '') {
