76580b13ce3d1933838afb4875809dfd5ffe6aaa
renetua <vasyl@typo3.net.ua>
2021-01-31 15:16:55 +0200

    Change #3187

---
+++ assets/js/main.m.js
@@ -0,0 +1,10 @@
+---
+layout: null
+sitemap: false
+---
+{%- include js/jquery.min.js -%}
+{%- include js/bootstrap.min.js -%}
+{%- include js/leaflet.js -%}
+{%- include js/leaflet.markercluster.js -%}
+{%- include js/L.Control.SlideMenu.js -%}
+{%- include js/realtyua.js -%}
