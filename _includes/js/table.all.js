19aa689093cb49985549e2a9b033cc37093fcdab
typo3ua <vasyl@typo3.net.ua>
2021-03-16 20:52:37 +0200

    Change #3667

--- _includes/js/table.all.js
+++ _includes/js/table.all.js
@@ -63,9 +63,6 @@ function jsDetailFormatter(index, row, $detail) {
   var d = new Date(row.date);
   var n = d.getMonth();
 
-  console.log(cDate);
-  console.log(row.date);
-
   $.each(row, function (key, value) {
     if (key !== 'images' || key !== 'id' && value !== '') {
       if (row.type.indexOf('{{ site.data.lang-uk.re_land }}') !== -1 || row.type.indexOf('{{ site.data.lang-uk.re_land | downcase }}') !== -1) {
