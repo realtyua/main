5f8539be847a396733357086bf1d80db66b24b6e
typo3ua <vasyl@typo3.net.ua>
2021-03-20 11:06:08 +0200

    Change #3899

--- _includes/js/realtyua.js
+++ _includes/js/realtyua.js
@@ -4,4 +4,16 @@ $("body").tooltip({selector:'[data-toggle="tooltip"]'});
 $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
+
+  // $(".btn-group .dropdown-toggle").on("click", function(event){
+  //     var liparent=$(this.parentElement);
+  //     var divChild=liparent.find('div');
+  //     var xOffset=liparent.offset().left;
+  //     var alignRight=($(document).width()-xOffset)<xOffset;
+  //     if (liparent.hasClass("dropdown-submenu")) {
+  //     } else {
+  //       divChild.toggleClass("dropdown-menu-right", alignRight);
+  //     }
+  // });
+
 });
