a782790d36f1a1fc5127bc159830c31ef7d28e7e
typo3ua <vasyl@typo3.net.ua>
2021-04-08 22:52:14 +0300

    Change #4122

--- _includes/js/realtyua.js
+++ _includes/js/realtyua.js
@@ -5,15 +5,15 @@ $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').colla
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
 
-  // $(".btn-group .dropdown-toggle").on("click", function(event){
-  //     var liparent=$(this.parentElement);
-  //     var divChild=liparent.find('div');
-  //     var xOffset=liparent.offset().left;
-  //     var alignRight=($(document).width()-xOffset)<xOffset;
-  //     if (liparent.hasClass("dropdown-submenu")) {
-  //     } else {
-  //       divChild.toggleClass("dropdown-menu-right", alignRight);
-  //     }
-  // });
+$(".btn-group .dropdown-toggle").on("click", function(event){
+    var liparent=$(this.parentElement);
+    var divChild=liparent.find('div');
+    var xOffset=liparent.offset().left;
+    var alignRight=($(document).width()-xOffset)<xOffset;
+    if (liparent.hasClass("dropdown-menu")) {
+    } else {
+      divChild.toggleClass("dropdown-menu-right", alignRight);
+    }
+});
 
 });
