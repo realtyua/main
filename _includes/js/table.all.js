cf60701a96ca47712174b234cf596af20bb9a962
realtyua <admin@realestate.if.ua>
2023-06-27 19:05:50 +0300

    Change #4712

--- _includes/js/table.all.js
+++ _includes/js/table.all.js
@@ -1,24 +1,16 @@
 
 $(function () {
   "use strict";
-  var $reObj = $('#property');
-  const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop), });
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
   let value = params.id;
   if (value && value !== '') {
     if (value.split('').length === 12) {
-      $reObj.bootstrapTable('filterBy', { phone: value });
+      $('#property').bootstrapTable('filterBy', { phone: value })
     } else {
-      $reObj.bootstrapTable('filterBy', { id: value });
-      $reObj.on('post-body.bs.table', (e) => {
-        const $trs = $(e.currentTarget).find('tbody tr')
-        if ($trs.length === 1) {
-          $reObj.bootstrapTable('toggleView');
-          $.each($trs, function (key, tr) {
-            $reObj.bootstrapTable('expandRow', $(tr).data('index'));
-            $(tr).addClass('active');
-          })
-        }
-      })
+      $('#property').bootstrapTable('filterBy', { id: value })
+      $('#property').bootstrapTable('toggleView')
     }
   }
   var expandedRow = null;
