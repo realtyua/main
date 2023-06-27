a87e85e04a350edbc24ed1c9c4e910a87ba826d6
realtyua <admin@realestate.if.ua>
2023-06-27 13:58:05 +0300

    Change #4709

--- _includes/js/table.all.js
+++ _includes/js/table.all.js
@@ -6,10 +6,10 @@ $(function () {
   let value = params.id;
   if (value && value !== '') {
     if (value.split('').length === 12) { $reObj.bootstrapTable('filterBy', { phone: value }); } else { $reObj.bootstrapTable('filterBy', { id: value }); }
-    $reObj.bootstrapTable('toggleView');
     $reObj.on('post-body.bs.table', (e) => {
       const $trs = $(e.currentTarget).find('tbody tr')
       if ($trs.length === 1) {
+        $reObj.bootstrapTable('toggleView');
         $.each($trs, function (key, tr) {
           $reObj.bootstrapTable('expandRow', $(tr).data('index'));
           $(tr).addClass('active');
--- _includes/js/table.js
+++ _includes/js/table.js
@@ -1,16 +1,15 @@
 
 $(function () {
   "use strict";
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
+  var $reObj = $('#property');
+  const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop), });
   let value = params.id;
   if (value && value !== '') {
     if (value.split('').length === 12) { $reObj.bootstrapTable('filterBy', { phone: value }); } else { $reObj.bootstrapTable('filterBy', { id: value }); }
-    $reObj.bootstrapTable('toggleView');
     $reObj.on('post-body.bs.table', (e) => {
       const $trs = $(e.currentTarget).find('tbody tr')
       if ($trs.length === 1) {
+        $reObj.bootstrapTable('toggleView');
         $.each($trs, function (key, tr) {
           $reObj.bootstrapTable('expandRow', $(tr).data('index'));
           $(tr).addClass('active');
