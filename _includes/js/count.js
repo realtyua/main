4bed780da44f4ce2edf47f8067d68586a1e135ca
TYPO3 Integrator <admin@realestate.if.ua>
2026-04-10 23:21:13 +0300

    Change #5138

--- _includes/js/count.js
+++ _includes/js/count.js
@@ -3,7 +3,7 @@
 
 	window.goatcounter = window.goatcounter || {};
 
-	var s = document.querySelector('script[data-goatcounter]')
+	var s = document.querySelector('script[data-goatcounter]');
 	if (s && s.dataset.goatcounterSettings) {
 		try         { var set = JSON.parse(s.dataset.goatcounterSettings) }
 		catch (err) { console.error('invalid JSON in data-goatcounter-settings: ' + err) }
@@ -12,7 +12,7 @@
 				window.goatcounter[k] = set[k]
 	}
 
-	var enc = encodeURIComponent
+	var enc = encodeURIComponent;
 
 	window.goatcounter.get_data = function(vars) {
 		vars = vars || {};
@@ -26,22 +26,22 @@
 			q: location.search,
 		};
 
-		var rcb, pcb, tcb
-		if (typeof(data.r) === 'function') rcb = data.r
-		if (typeof(data.t) === 'function') tcb = data.t
-		if (typeof(data.p) === 'function') pcb = data.p
+		var rcb, pcb, tcb;
+	if (typeof(data.r) === 'function') rcb = data.r;
+	if (typeof(data.t) === 'function') tcb = data.t;
+	if (typeof(data.p) === 'function') pcb = data.p;
 
-		if (is_empty(data.r)) data.r = document.referrer
-		if (is_empty(data.t)) data.t = document.title
-		if (is_empty(data.p)) data.p = get_path()
+	if (is_empty(data.r)) data.r = document.referrer;
+	if (is_empty(data.t)) data.t = document.title;
+	if (is_empty(data.p)) data.p = get_path();
 
-		if (rcb) data.r = rcb(data.r)
-		if (tcb) data.t = tcb(data.t)
-		if (pcb) data.p = pcb(data.p)
-		return data
-	}
+	if (rcb) data.r = rcb(data.r);
+	if (tcb) data.t = tcb(data.t);
+	if (pcb) data.p = pcb(data.p);
+	return data;
+	};
 
-	var is_empty = function(v) { return v === null || v === undefined || typeof(v) === 'function' }
+	var is_empty = function(v) { return v === null || v === undefined || typeof(v) === 'function' };
 
 	var is_bot = function() {
 		var w = window, d = document
@@ -54,107 +54,107 @@
 		if (navigator.webdriver)
 			return 153
 		return 0
-	}
+	};
 
 	var urlencode = function(obj) {
-		var p = []
+		var p = [];
 		for (var k in obj)
 			if (obj[k] !== '' && obj[k] !== null && obj[k] !== undefined && obj[k] !== false)
 				p.push(enc(k) + '=' + enc(obj[k]))
 		return '?' + p.join('&')
-	}
+	};
 
 	var warn = function(msg) {
 		if (console && 'warn' in console)
 			console.warn('goatcounter: ' + msg)
-	}
+	};
 
 	var get_endpoint = function() {
-		var s = document.querySelector('script[data-goatcounter]')
+		var s = document.querySelector('script[data-goatcounter]');
 		return (s && s.dataset.goatcounter) ? s.dataset.goatcounter : goatcounter.endpoint
-	}
+	};
 
 	var get_path = function() {
 		var loc = location,
-			c = document.querySelector('link[rel="canonical"][href]')
+			c = document.querySelector('link[rel="canonical"][href]');
 		if (c) {
-			var a = document.createElement('a')
-			a.href = c.href
+			var a = document.createElement('a');
+			a.href = c.href;
 			if (a.hostname.replace(/^www\./, '') === location.hostname.replace(/^www\./, ''))
-				loc = a
+				loc = a;
 		}
-		return (loc.pathname + loc.search) || '/'
-	}
+		return (loc.pathname + loc.search) || '/';
+	};
 
 	var on_load = function(f) {
 		if (document.body === null)
-			document.addEventListener('DOMContentLoaded', function() { f() }, false)
+			document.addEventListener('DOMContentLoaded', function() { f() }, false);
 		else
-			f()
-	}
+			f();
+	};
 
 	window.goatcounter.filter = function() {
 		if ('visibilityState' in document && document.visibilityState === 'prerender')
-			return 'visibilityState'
+			return 'visibilityState';
 		if (!goatcounter.allow_frame && location !== parent.location)
-			return 'frame'
+			return 'frame';
 		if (!goatcounter.allow_local && location.hostname.match(/(localhost$|^127\.|^10\.|^172\.(1[6-9]|2[0-9]|3[0-1])\.|^192\.168\.|^0\.0\.0\.0$)/))
-			return 'localhost'
+			return 'localhost';
 		if (!goatcounter.allow_local && location.protocol === 'file:')
-			return 'localfile'
+			return 'localfile';
 		if (localStorage && localStorage.getItem('skipgc') === 't')
-			return 'disabled with #toggle-goatcounter'
-		return false
-	}
+			return 'disabled with #toggle-goatcounter';
+		return false;
+	};
 
 	window.goatcounter.url = function(vars) {
 		var data = window.goatcounter.get_data(vars || {});
 		if (data.p === null)
-			return
-		data.rnd = Math.random().toString(36).substr(2, 5)
+			return;
+		data.rnd = Math.random().toString(36).substr(2, 5);
 
-		var endpoint = get_endpoint()
+		var endpoint = get_endpoint();
 		if (!endpoint)
-			return warn('no endpoint found')
+			return warn('no endpoint found');
 
-		return endpoint + urlencode(data)
-	}
+		return endpoint + urlencode(data);
+	};
 
 	window.goatcounter.count = function(vars) {
-		var f = goatcounter.filter()
+		var f = goatcounter.filter();
 		if (f)
-			return warn('not counting because of: ' + f)
-		var url = goatcounter.url(vars)
+			return warn('not counting because of: ' + f);
+		var url = goatcounter.url(vars);
 		if (!url)
-			return warn('not counting because path callback returned null')
+			return warn('not counting because path callback returned null');
 
 		if (!navigator.sendBeacon(url)) {
-			var img = document.createElement('img')
-			img.src = url
-			img.style.position = 'absolute'
-			img.style.bottom = '0px'
-			img.style.width = '1px'
-			img.style.height = '1px'
-			img.loading = 'eager'
-			img.setAttribute('alt', '')
-			img.setAttribute('aria-hidden', 'true')
+			var img = document.createElement('img');
+			img.src = url;
+			img.style.position = 'absolute';
+			img.style.bottom = '0px';
+			img.style.width = '1px';
+			img.style.height = '1px';
+			img.loading = 'eager';
+			img.setAttribute('alt', '');
+			img.setAttribute('aria-hidden', 'true');
 
-			var rm = function() { if (img && img.parentNode) img.parentNode.removeChild(img) }
-			img.addEventListener('load', rm, false)
-			document.body.appendChild(img)
+			var rm = function() { if (img && img.parentNode) img.parentNode.removeChild(img) };
+			img.addEventListener('load', rm, false);
+			document.body.appendChild(img);
 		}
-	}
+	};
 
 	window.goatcounter.get_query = function(name) {
-		var s = location.search.substr(1).split('&')
+		var s = location.search.substr(1).split('&');
 		for (var i = 0; i < s.length; i++)
 			if (s[i].toLowerCase().indexOf(name.toLowerCase() + '=') === 0)
-				return s[i].substr(name.length + 1)
-	}
+				return s[i].substr(name.length + 1);
+	};
 
 	window.goatcounter.bind_events = function() {
 		if (!document.querySelectorAll)
-			return
+			return;
 
 		var send = function(elem) {
 			return function() {
@@ -163,80 +163,80 @@
 					path:     (elem.dataset.goatcounterClick || elem.name || elem.id || ''),
 					title:    (elem.dataset.goatcounterTitle || elem.title || (elem.innerHTML || '').substr(0, 200) || ''),
 					referrer: (elem.dataset.goatcounterReferrer || elem.dataset.goatcounterReferral || ''),
-				})
+				});
 			}
-		}
+		};
 
 		Array.prototype.slice.call(document.querySelectorAll("*[data-goatcounter-click]")).forEach(function(elem) {
 			if (elem.dataset.goatcounterBound)
-				return
-			var f = send(elem)
-			elem.addEventListener('click', f, false)
-			elem.addEventListener('auxclick', f, false)
-			elem.dataset.goatcounterBound = 'true'
-		})
-	}
+				return;
+			var f = send(elem);
+			elem.addEventListener('click', f, false);
+			elem.addEventListener('auxclick', f, false);
+			elem.dataset.goatcounterBound = 'true';
+		});
+	};
 
 	window.goatcounter.visit_count = function(opt) {
 		on_load(function() {
-			opt        = opt        || {}
-			opt.type   = opt.type   || 'html'
-			opt.append = opt.append || 'body'
-			opt.path   = opt.path   || get_path()
-			opt.attr   = opt.attr   || {width: '200', height: (opt.no_branding ? '60' : '80')}
+			opt        = opt        || {};
+			opt.type   = opt.type   || 'html';
+			opt.append = opt.append || 'body';
+			opt.path   = opt.path   || get_path();
+			opt.attr   = opt.attr   || {width: '200', height: (opt.no_branding ? '60' : '80')};
 
-			opt.attr['src'] = get_endpoint() + 'er/' + enc(opt.path) + '.' + enc(opt.type) + '?'
-			if (opt.no_branding) opt.attr['src'] += '&no_branding=1'
-			if (opt.style)       opt.attr['src'] += '&style=' + enc(opt.style)
-			if (opt.start)       opt.attr['src'] += '&start=' + enc(opt.start)
-			if (opt.end)         opt.attr['src'] += '&end='   + enc(opt.end)
+			opt.attr['src'] = get_endpoint() + 'er/' + enc(opt.path) + '.' + enc(opt.type) + '?';
+			if (opt.no_branding) opt.attr['src'] += '&no_branding=1';
+			if (opt.style)       opt.attr['src'] += '&style=' + enc(opt.style);
+			if (opt.start)       opt.attr['src'] += '&start=' + enc(opt.start);
+			if (opt.end)         opt.attr['src'] += '&end='   + enc(opt.end);
 
 			var tag = {png: 'img', svg: 'img', html: 'iframe'}[opt.type];
 			if (!tag)
-				return warn('visit_count: unknown type: ' + opt.type)
+				return warn('visit_count: unknown type: ' + opt.type);
 
 			if (opt.type === 'html') {
-				opt.attr['frameborder'] = '0'
-				opt.attr['scrolling']   = 'no'
+				opt.attr['frameborder'] = '0';
+				opt.attr['scrolling']   = 'no';
 			}
 
-			var d = document.createElement(tag)
+			var d = document.createElement(tag);
 			for (var k in opt.attr)
-				d.setAttribute(k, opt.attr[k])
+				d.setAttribute(k, opt.attr[k]);
 
-			var p = document.querySelector(opt.append)
+			var p = document.querySelector(opt.append);
 			if (!p)
-				return warn('visit_count: element to append to not found: ' + opt.append)
-			p.appendChild(d)
-		})
-	}
+				return warn('visit_count: element to append to not found: ' + opt.append);
+			p.appendChild(d);
+		});
+	};
 
 	if (location.hash === '#toggle-goatcounter') {
 		if (localStorage.getItem('skipgc') === 't') {
-			localStorage.removeItem('skipgc', 't')
-			alert('GoatCounter tracking is now ENABLED in this browser.')
+			localStorage.removeItem('skipgc', 't');
+			alert('GoatCounter tracking is now ENABLED in this browser.');
 		}
 		else {
-			localStorage.setItem('skipgc', 't')
-			alert('GoatCounter tracking is now DISABLED in this browser until ' + location + ' is loaded again.')
+			localStorage.setItem('skipgc', 't');
+			alert('GoatCounter tracking is now DISABLED in this browser until ' + location + ' is loaded again.');
 		}
 	}
 
 	if (!goatcounter.no_onload)
 		on_load(function() {
 			if (!('visibilityState' in document) || document.visibilityState === 'visible')
-				goatcounter.count()
+				goatcounter.count();
 			else {
 				var f = function(e) {
 					if (document.visibilityState !== 'visible')
-						return
-					document.removeEventListener('visibilitychange', f)
-					goatcounter.count()
-				}
-				document.addEventListener('visibilitychange', f)
+						return;
+					document.removeEventListener('visibilitychange', f);
+					goatcounter.count();
+				};
+				document.addEventListener('visibilitychange', f);
 			}
 
 			if (!goatcounter.no_events)
-				goatcounter.bind_events()
-		})
+				goatcounter.bind_events();
+		});
 })();
